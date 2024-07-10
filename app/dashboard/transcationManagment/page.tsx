e2987f74d9dc1d8ba1ea9a61e09e65849af1212e
muhammad ismaeel <muhammad.ismaeel5162@gmail.com>
2024-07-10 16:07:53 +0500

    first commit

---
+++ app/dashboard/transcationManagment/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Transcation() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Transcation Managment </h1>
+    </main>
+  );
+}
